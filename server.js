3b8af75ea23327637e8badb28a64cad0fb695256
Quyen Dong <44275983+quyendong@users.noreply.github.com>
2018-10-29 18:16:59 -0500

    Add files via upload
    Initial commit. Server running concurrently with react app.

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const flash = require('connect-flash');
+const cors = require('cors');
+const crypto = require('crypto');
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'quyen',
+  database : 'simplygrow'
+});
+
+//start mysql connection
+ connection.connect(function(err) {
+    if (err) throw err
+      console.log('You are now connected...')
+ });
+ 
+// Initialize the app
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(flash());
+app.use(cors());
+
+// Start the server
+app.listen(4000, () => {
+ console.log('Server is running.');
+});
